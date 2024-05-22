04675f202a7cd7c116d18999621cd51e892ca9f8
Dustin Secrest <dustin.secrest@gmail.com>
2024-05-22 10:14:31 -0700

    Delete storybook-static directory

--- storybook-static/assets/Avatar.stories-BKZmpxp9.js
+++
@@ -1,8 +0,0 @@
-import{R as u}from"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";const l=({imgAlt:i,imgSrc:n,size:m="md",status:a,...o})=>u.createElement("img",{src:n,alt:i,className:["t-avatar",`t-avatar--${m}`,a&&`t-avatar--${a}`].join(" "),...o});l.__docgenInfo={description:"",methods:[],displayName:"Avatar",props:{imgAlt:{required:!0,tsType:{name:"string"},description:""},imgSrc:{required:!0,tsType:{name:"string"},description:""},size:{required:!1,tsType:{name:"union",raw:"'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl'",elements:[{name:"literal",value:"'xs'"},{name:"literal",value:"'sm'"},{name:"literal",value:"'md'"},{name:"literal",value:"'lg'"},{name:"literal",value:"'xl'"},{name:"literal",value:"'xxl'"}]},description:"",defaultValue:{value:"'md'",computed:!1}},status:{required:!1,tsType:{name:"union",raw:'"default" | "error" | "success"',elements:[{name:"literal",value:'"default"'},{name:"literal",value:'"error"'},{name:"literal",value:'"success"'}]},description:""}}};const p={component:l,parameters:{layout:"centered",actions:{disable:!0},interactions:{disable:!0}}},e={args:{size:"xxl",imgAlt:"Someone's profile pic",imgSrc:"https://placebeard.it/250/250",status:"default"}};var t,r,s;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  args: {
-    size: "xxl",
-    imgAlt: "Someone's profile pic",
-    imgSrc: "https://placebeard.it/250/250",
-    status: "default"
-  }
-}`,...(s=(r=e.parameters)==null?void 0:r.docs)==null?void 0:s.source}}};const f=["Default"];export{e as Default,f as __namedExportsOrder,p as default};
